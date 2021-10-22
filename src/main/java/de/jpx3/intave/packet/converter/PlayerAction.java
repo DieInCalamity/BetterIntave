5381c7a257205e6bba6aec499d4cffab45617167
ventolotl <ventolotl@users.noreply.github.com>
2021-10-22 23:29:47 +0200

    optimize PlayerAction

--- src/main/java/de/jpx3/intave/packet/converter/PlayerAction.java
+++ src/main/java/de/jpx3/intave/packet/converter/PlayerAction.java
@@ -4,24 +4,18 @@ import de.jpx3.intave.annotate.KeepEnumInternalNames;
 
 @KeepEnumInternalNames
 public enum PlayerAction {
-  PRESS_SHIFT_KEY("PRESS_SHIFT_KEY"),
-  RELEASE_SHIFT_KEY("RELEASE_SHIFT_KEY"),
-  START_SNEAKING("START_SNEAKING"),
-  STOP_SNEAKING("STOP_SNEAKING"),
-  STOP_SLEEPING("STOP_SLEEPING"),
-  START_SPRINTING("START_SPRINTING"),
-  STOP_SPRINTING("STOP_SPRINTING"),
-  START_RIDING_JUMP("START_RIDING_JUMP"),
-  STOP_RIDING_JUMP("STOP_RIDING_JUMP"),
-  OPEN_INVENTORY("OPEN_INVENTORY"),
-  START_FALL_FLYING("START_FALL_FLYING"),
-  RIDING_JUMP("RIDING_JUMP");
-
-  private final String action;
-
-  PlayerAction(String action) {
-    this.action = action;
-  }
+  PRESS_SHIFT_KEY,
+  RELEASE_SHIFT_KEY,
+  START_SNEAKING,
+  STOP_SNEAKING,
+  STOP_SLEEPING,
+  START_SPRINTING,
+  STOP_SPRINTING,
+  START_RIDING_JUMP,
+  STOP_RIDING_JUMP,
+  OPEN_INVENTORY,
+  START_FALL_FLYING,
+  RIDING_JUMP;
 
   public boolean isSneaking() {
     switch (this) {
@@ -33,9 +27,5 @@ public enum PlayerAction {
       default:
         return false;
     }
-  }
-
-  public String action() {
-    return action;
   }
 }
