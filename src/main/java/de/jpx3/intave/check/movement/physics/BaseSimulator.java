17b2c42dadf340a33ef0656c154eaf1892945643
Lennox <admin@lennoxlotl.dev>
2023-07-14 01:11:09 +0200

    fix: the biggest skill issue ever

--- src/main/java/de/jpx3/intave/check/movement/physics/BaseSimulator.java
+++ src/main/java/de/jpx3/intave/check/movement/physics/BaseSimulator.java
@@ -651,7 +651,7 @@ class BaseSimulator extends Simulator {
       double distanceToCenter = distanceToCenter(x, y, z, positionX, positionY, positionZ);
       int comparison = compare(x, y, z, blockX, blockY, blockZ);
 
-      if (distanceToCenter < distance || distanceToCenter == distance || comparison < 0) {
+      if (distanceToCenter < distance || distanceToCenter == distance && comparison < 0) {
         block = VolatileBlockAccess.typeAccess(user, world, x, y, z);
         blockX = x;
         blockY = y;
@@ -673,7 +673,7 @@ class BaseSimulator extends Simulator {
       // Entity#getOnPos(float)
       boolean requiresBlockBelow = !block.name().contains("FENCE") && !block.name().contains("WALL");
       if (requiresBlockBelow) {
-        block = VolatileBlockAccess.typeAccess(user, world, blockX, blockY - 0.2, blockZ);
+        block = VolatileBlockAccess.typeAccess(user, world, blockX, ClientMath.floor(environment.positionY() - 0.2), blockZ);
       }
     }
 
