c356034acd8799785b9dc658ca3654257f72782b
Richy <strunk876@gmail.com>
2023-09-21 23:05:17 +0200

    Update 14.6.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val simpleName = "Intave"
 group = "de.jpx3"
 version = "14.6.6"
-description = "Cheat detection software, providing fair play"
+description = "Automated cheat detection and prevention"
 
 /*
  * Dependencies
