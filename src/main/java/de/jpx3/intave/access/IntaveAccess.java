5abc0bb4c5e6a92026363ddc080be239d2c859a8
DieInCalamity <160578963+DieInCalamity@users.noreply.github.com>
2026-04-25 08:41:41 +0700

    test build

--- src/main/java/de/jpx3/intave/access/IntaveAccess.java
+++ src/main/java/de/jpx3/intave/access/IntaveAccess.java
@@ -20,7 +20,7 @@ import java.lang.ref.WeakReference;
  * It will be made available to the user either via {@link IntaveAccessor#unsafeAccess()}
  * or via {@link WeakReference} at {@link IntaveAccessor#weakAccess()}.
  */
-public interface IntaveAccess { // test build
+public interface IntaveAccess {
 
   /**
    * Overrides the global trustfactor resolver with a new one.
